a576fc34726fc0b31a60d00ca67c275089a0dba8
Connor <61102529+Metater@users.noreply.github.com>
2023-11-18 19:49:40 -0500

    It works!

--- category5scoutingv2.client/vite.config.ts
+++ category5scoutingv2.client/vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/weatherforecast': {
-                target: 'https://localhost:5174/',
+                target: 'http://localhost:5169/',
                 secure: false
             }
         },
